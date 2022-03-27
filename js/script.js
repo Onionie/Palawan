de761b7f275017d09890dcea8fe033db129dab8f
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-03-26 20:30:40 -0700

    Smooth Scrolling

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
-
 // Make mobile navigation work
-
 const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
 
@@ -10,7 +6,37 @@ btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
 
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
 checkFlexGap();
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gaap property missing in some Safari versions
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
